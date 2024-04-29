0c581ecd2b3c69b57b49e2918807ac265c251bea
Pavel Chervenyashki <pavel.chervenyashki@codexio.bg>
2024-04-29 15:49:36 +0300

    Coding style refactor

--- src/main/java/bg/codexio/springframework/data/jpa/requery/resolver/function/ExtendedMysqlDialect.java
+++ src/main/java/bg/codexio/springframework/data/jpa/requery/resolver/function/ExtendedMysqlDialect.java
@@ -14,9 +14,9 @@ public class ExtendedMysqlDialect
         super.initializeFunctionRegistry(functionContributions);
 
         functionContributions.getFunctionRegistry()
-                .register(
-                        CaseInsensitiveLikeSQLFunction.FUNC_NAME,
-                        new CaseInsensitiveLikeSQLFunction()
-                );
+                             .register(
+                                     CaseInsensitiveLikeSQLFunction.FUNC_NAME,
+                                     new CaseInsensitiveLikeSQLFunction()
+                             );
     }
 }
