5015fb52656f10e47bb7cdd5630f83598d50a0ae
Pavel Chervenyashki <pavel.chervenyashki@codexio.bg>
2024-05-10 07:39:59 +0300

    Moved and renamed dialects, updated version to most recent release ones and added filter examples in the documentation

--- src/main/java/bg/codexio/springframework/data/jpa/requery/dialect/RequeryEnhancedPostgreSQLDialect.java
+++ src/main/java/bg/codexio/springframework/data/jpa/requery/dialect/RequeryEnhancedPostgreSQLDialect.java
@@ -1,14 +1,15 @@
-package bg.codexio.springframework.data.jpa.requery.resolver.function;
+package bg.codexio.springframework.data.jpa.requery.dialect;
 
+import bg.codexio.springframework.data.jpa.requery.resolver.function.CaseInsensitiveLikeSQLFunction;
 import org.hibernate.boot.model.FunctionContributions;
-import org.hibernate.dialect.MySQLDialect;
+import org.hibernate.dialect.PostgreSQLDialect;
 
 /**
- * Extends the standard {@link MySQLDialect} and registers a custom function
- * to support case-insensitive LIKE operations.
+ * Extends the standard {@link PostgreSQLDialect} and registers a custom
+ * function to support case-insensitive LIKE operations.
  */
-public class ExtendedMysqlDialect
-        extends MySQLDialect {
+public class RequeryEnhancedPostgreSQLDialect
+        extends PostgreSQLDialect {
     @Override
     public void initializeFunctionRegistry(FunctionContributions functionContributions) {
         super.initializeFunctionRegistry(functionContributions);
